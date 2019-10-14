2b80822ae8b32303b846567bd4954787d072ecdf
hda <hda@hdadeiMac.local>
2019-10-14 14:23:48 +0800

    add a,h

---
+++ Teamlib.h
@@ -0,0 +1,13 @@
+//
+//  Teamlib.h
+//  Teamlib
+//
+//  Created by hda on 2019/10/14.
+//  Copyright © 2019 onlyou.pw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Teamlib : NSObject
+-(void)hellolib;
+@end
